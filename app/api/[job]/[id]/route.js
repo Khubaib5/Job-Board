b6f87f6416216b0e1018365841b8d030b50581ed
Khubaib <khubaibwasim75@gmail.com>
2024-04-26 15:31:34 +0500

    first commit

--- app/api/[job]/[id]/route.js
+++ app/api/[job]/[id]/route.js
@@ -1,20 +1,21 @@
-import { prisma } from "../../../../lib/prisma";
-import { NextRequest, NextResponse } from "next/server";
+import {prisma} from "@/lib/prisma"
+import { NextRequest, NextResponse } from "next/server"
 
-export const GET = async ({params})=>{
-    const {id} = params
-    try {
-        const post = await prisma.jobPosting.findUnique({
-          where: { id },
-        });
-    
-        return new NextResponse(JSON.stringify(post, null, 2), { status: 200 });
-      } catch (err) {
-        console.log(err);
-        return new NextResponse(
-          JSON.stringify({ message: "Something went wrong!" }, null, 2),
-          { status: 500 }
-        );
-      }
 
+export const GET = async (req, {params}) => {
+  const {id} = params
+
+  try {
+    const post = await prisma.jobPosting.findUnique({
+      where: { id },
+    });
+
+    return new NextResponse(JSON.stringify(post, null, 2), { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }, null, 2),
+      { status: 500 }
+    );
+  }
 }
